519a26f6d5b2bf115392bebb1c3f4ebc2349b0d6
Ana Claudia <a.claudia@estudante.ifro.edu.br>
2024-04-12 17:03:46 -0400

    Primeiro commit

---
+++ main.py
@@ -0,0 +1,10 @@
+# Este é um objeto de estudo para aprender a linguagem de programação Python
+
+print('Hello Word, vamos lá!')
+
+# Atribuição de variáveis
+vendas = 1000 # variável recebe
+custo = 300
+lucro = vendas - custo
+
+print('O lucro foi de:', lucro)
