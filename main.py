3db2ff4f678e7ee7a3d1d5f200565b6d461812f5
Ana Claudia <a.claudia@estudante.ifro.edu.br>
2024-04-17 11:22:17 -0400

    Realizando novos desafios para aprimoração do conhecimento e prática

--- main.py
+++ main.py
@@ -12,9 +12,39 @@ print('O lucro foi de:', lucro)
 
 
 #Atividades:
+
+#Desafio 1:
 """Criar um script em Python que leia o nome de uma pessoa e mostre uma mensagem de boas-vindas de acordo com o valor digitado."""
 
 #resultado
 nome = input('Qual o seu nome?')
 print('Olá ' + nome + ' seja bem-vindo(a)!')
 
+##################################################
+
+#Desafio 2:
+"""Criar um script em Python que leia o dia, o mês e o ano de nascimento de uma pessoa e mostre uma mensagem com a data formatada."""
+
+#resultado
+print('Insira as informações abaixo:')
+dia = input('Dia = ')
+mes = input('Mês = ')
+ano = input('Ano = ')
+print('Você nasceu no dia ' + dia + ' de ' + mes + ' de ' + ano + '. Correto?')
+
+##################################################
+
+#Desafio 3
+"""Criar um script em Python que leia dois números e tente mostrar a soma entre eles."""
+
+#resultado
+print('Insira os números para soma:')
+primeiro_numero = input('Primeiro Número = ')
+segundo_numero = input('Segundo Número = ')
+
+#Convertendo para tipo numérico
+primeiro_numero = int(primeiro_numero)
+segundo_numero = int(segundo_numero)
+
+resultado = (primeiro_numero + segundo_numero)
+print('O resultado da soma é: ' + str(resultado)) #concatenando e convertendo para string novamente com str()
